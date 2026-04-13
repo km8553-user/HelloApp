1a57f61b9489bae9afa121199aa7ba8dd46dcca2
km8553-user <km8553@srmist.edu.in>
2026-04-13 08:44:03 +0530

    modified

--- Hello_App/src/main/java/org/example/HelloApp.java
+++ Hello_App/src/main/java/org/example/HelloApp.java
@@ -1,5 +1,24 @@
-class HelloApp {
+public class HelloApp {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        StringBuilder names = new StringBuilder();
+
+        if (args.length > 0) {
+
+            for (int i = 0; i < args.length; i++) {
+                names.append(args[i]);
+
+                // Add comma except for last element
+                if (i < args.length - 1) {
+                    names.append(", ");
+                }
+            }
+
+            System.out.println("Hello, " + names.toString() + "!");
+
+        } else {
+            System.out.println("Hello, World!");
+        }
     }
 }
